bf908a48ef33a04a5948a8cabbf1defff296c8fa
Nivethasadagopan <101561061+Nivethasadagopan@users.noreply.github.com>
2025-11-07 14:53:46 +0000

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-<<<<<<< HEAD
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-=======
-export default defineConfig({
-  plugins: [react()],
   base: '/',
->>>>>>> 1a14ddbaa6767901ff23f03e3a6e8ff9350ce658
-})
+});
